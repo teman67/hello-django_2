50b76c9a6f2dfeb589edd6115d0d920f9701533b
Amirhossein Bayani <amirhossein.bayani@gmail.com>
2023-11-14 17:38:09 +0000

    add todo_app and test django

--- todo/settings.py
+++ todo/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-0_jn)1wgdvo3z^@2_1^4l8t_!4%q3y5k+-t-!clvvovhe+(t-0'
+SECRET_KEY = 'hasaninagobalabego_14087_AhB-!@#'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
